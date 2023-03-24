7a113202640b94c9f579108ae3ba55dd008a9aeb
Vitalii Karpiv <vitalii.karpiv@plus4u.net>
2023-03-24 10:44:48 +0200

    Initial commit

---
+++ src/main/java/com/vka/prix/PrixApplication.java
@@ -0,0 +1,13 @@
+package com.vka.prix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vka/prix/PrixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vka.prix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
